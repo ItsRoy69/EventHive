460dadbd4b54bf447a014f98eadafebcfd97be4a
Srinkk <shreyaganguly2003@gmail.com>
2024-05-19 23:58:49 +0530

    Update Events Page

--- constants/icons.js
+++ constants/icons.js
@@ -2,11 +2,29 @@ import activity from '../assets/icons/activity.png'
 import calendar from '../assets/icons/calendar.png'
 import dm from '../assets/icons/dm.png'
 import event from '../assets/icons/event.png'
+import ham from '../assets/icons/ham.png'
+import line from '../assets/icons/line.png'
+import search from '../assets/icons/search.png'
+import menu from '../assets/icons/menu.png'
+import invite from '../assets/icons/invite.png'
+import groupChat from '../assets/icons/groupChat.png'
+import florist from '../assets/icons/florist.png'
+import venue from '../assets/icons/venue.png'
+import phone from '../assets/icons/phone.png'
 
 
 export default{
     activity,
     calendar,
     dm,
-    event
+    event,
+    ham,
+    search,
+    line,
+    menu,
+    invite,
+    groupChat,
+    florist,
+    venue,
+    phone
 }
