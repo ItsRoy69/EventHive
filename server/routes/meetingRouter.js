acf2527fd97cc5cab0aab921ca8e030fefc7aaf4
Rajarshi Dutta <gogolmf74@gmail.com>
2024-06-02 10:59:38 +0530

    fix meeting get route

--- server/routes/meetingRouter.js
+++ server/routes/meetingRouter.js
@@ -3,7 +3,7 @@ const meetingController = require('../controllers/meetingController')
 const { verifyToken } = require('../middlewares/auth')
 const { getUserRoleInEvent } = require('../middlewares/role')
 
-router.get('/', verifyToken, getUserRoleInEvent, meetingController.getMeetings)
+router.get('/:eventId', verifyToken, getUserRoleInEvent, meetingController.getMeetings)
 router.post('/', verifyToken, getUserRoleInEvent, meetingController.createMeeting)
 router.put('/', verifyToken, getUserRoleInEvent, meetingController.updateMeeting)
 router.delete('/', verifyToken, getUserRoleInEvent, meetingController.deleteMeeting) 
